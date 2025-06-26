07981ebd2884f212a31b2c342f32d5eb71c3d5a0
Ronmarche1114 <ronmarcherhyssquy@gmail.com>
2025-06-26 23:02:07 +0800

    commit now

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 // app/page.tsx
 import { redirect } from "next/navigation";
-// import AuthLogin from "@/app/auth/login/page";
-import LandingPage from "@/app/pages/landingPage/page";
+
+import AuthLogin from "@/app/auth/login/page";
 
 export default function Home() {
-  ``;
   return (
-    <main className="min-h-screen min-w-full">
-      <LandingPage />
+    <main className="min-h-screen min-w-screen">
+      <AuthLogin />
     </main>
   );
 }
